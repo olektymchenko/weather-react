2e2c6b1ca882867a669a5400b2dd3dd4e7d7aa59
olektymchenko <58951697+olektymchenko@users.noreply.github.com>
2020-03-22 14:42:30 +0100

    Updates

---
+++ precache-manifest.a865e9ccebce580f6f3dac154ee95323.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd45f4aafe1e27634d82d6c24a7c30f0",
+    "url": "/nazwa/index.html"
+  },
+  {
+    "revision": "74c548ccbe7a66c76590",
+    "url": "/nazwa/static/css/main.8d16ebc3.chunk.css"
+  },
+  {
+    "revision": "a9d539e463e3495e852f",
+    "url": "/nazwa/static/js/2.ca885dbe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/nazwa/static/js/2.ca885dbe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74c548ccbe7a66c76590",
+    "url": "/nazwa/static/js/main.9ca5402a.chunk.js"
+  },
+  {
+    "revision": "289b79b035d26c6af0e1",
+    "url": "/nazwa/static/js/runtime-main.359a1a00.js"
+  }
+]);
